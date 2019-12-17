20bfb93408945eaa59a039497dd2dcc1ab45c9b7
TS22082 <ts22082@gmail.com>
2019-12-16 21:31:26 -0800

    finished backend

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,10 +10,27 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  console.log("getting single todo");
   db.seeTodo(req.params.id, result => {
     res.send(result);
   });
+});
+
+router.post("/", (req, res) => {
+  const message = req.body.text;
+  db.createTodo(message);
+  res.send("success");
+});
+
+router.delete("/:id", (req, res) => {
+  console.log(req.params);
+  db.deleteTodo(req.params.id);
+  res.send("success");
+});
+
+router.patch("/", (req, res) => {
+  console.log(req.body);
+  db.updateTodo(req.body.id, req.body.text);
+  res.send("success");
 });
 
 module.exports = router;
